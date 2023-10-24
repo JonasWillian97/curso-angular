74ad1b3728256ac898190e505be85819bc69c206
Jonas Willian <jwnarancity@gmail.com>
2023-10-24 16:23:20 -0300

    Trabalhando com interpolação

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./first-component.component.css']
 })
 export class FirstComponentComponent {
+  name: string = "Jonas"
+  age: number = 26;
+  job: string = "Programador";
+  hobbies = ["Jogar Futebol", "Passear", "Estudar"];
+  car = {
+    name: "golf",
+    year: 2013
+  } 
 
 }
