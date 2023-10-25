4aab5d964601041fdc7767206bc0471d7667fa4f
Jonas Willian <jwnarancity@gmail.com>
2023-10-25 19:35:36 -0300

    Trabalhando com two way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { EmitterComponent } from './components/emitter/emitter.component';
 import { ChangeNumberComponent } from './components/change-number/change-number.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
 import { PipesComponent } from './components/pipes/pipes.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -26,10 +28,12 @@ import { PipesComponent } from './components/pipes/pipes.component';
     ChangeNumberComponent,
     ListRenderComponent,
     PipesComponent,
+    TwoWayBindingComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
